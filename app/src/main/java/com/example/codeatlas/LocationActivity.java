efe233ec468d8abb848aeeb7515e98eac4f9ddfb
FatimaZah7 <101220079@mu.edu.lb>
2024-05-31 09:20:35 +0300

    location

--- app/src/main/java/com/example/codeatlas/LocationActivity.java
+++ app/src/main/java/com/example/codeatlas/LocationActivity.java
@@ -6,13 +6,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -26,6 +26,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
@@ -38,23 +39,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.api.Context;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class LocationActivity extends BaseActivity  {
+public class LocationActivity extends BaseActivity implements LocationListener {
 
     final int PERMISSION_REQUEST_LOCATION = 101;
     Button skipBtn;
     ImageButton useCurrentLocationButton, setLocationOnMapBtn;
     EditText locationEditText;
-    GoogleMap googleMap;
-    private FusedLocationProviderClient fusedLocationClient;
-    private LocationRequest locationRequest;
-    private LocationCallback locationCallback;
-    Location currentLocation;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +58,8 @@ public class LocationActivity extends BaseActivity  {
         setContentView(R.layout.activity_location);
         initLayoutComponents();
         setupBackButton();
-        //initButtons();
+
+        initButtons();
         //initMap();
 
     }
@@ -79,6 +76,78 @@ public class LocationActivity extends BaseActivity  {
                 startActivity(intent);
             }
         });
+    }
+
+    private void initButtons() {
+        useCurrentLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestLocationPermission();
+            }
+        });
+    }
+
+    private void requestLocationPermission() {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                getLocation();
+            }
+            else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                    Snackbar.make(findViewById(R.id.activity_location),
+                                    "Code Atlas app requires this permission to locate your position",
+                                    Snackbar.LENGTH_INDEFINITE)
+                            .setAction("OK", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    ActivityCompat.requestPermissions(LocationActivity.this,
+                                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                                            PERMISSION_REQUEST_LOCATION);
+                                }
+                            });
+                }
+                else {
+                    ActivityCompat.requestPermissions(LocationActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                            PERMISSION_REQUEST_LOCATION);
+                }
+            }
+    }
+
+    private void getLocation(){
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    5000L, 10F, (android.location.LocationListener) this);
+
+        } catch (SecurityException e) {
+            Toast.makeText(this, "Can't get Location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        String userLocationName;
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                Address address = addresses.get(0);
+                userLocationName = address.getLocality();
+                locationEditText.setText(userLocationName);
+                saveLoCationIntoFirebase();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            userLocationName = "Lat: " + location.getLatitude() + ", Long: " + location.getLongitude();
+            locationEditText.setText(userLocationName);
+            saveLoCationIntoFirebase();
+        }
+    }
+
+    private void saveLoCationIntoFirebase() {
+        // save location
     }
 
     /*
